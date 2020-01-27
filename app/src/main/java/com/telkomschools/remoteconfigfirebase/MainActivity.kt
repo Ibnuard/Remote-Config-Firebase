9784d1c01374c4fcfa357002dc4426685cbc6d29
Ibnu Putra <azxarblog@gmail.com>
2020-01-27 08:53:25 +0700

    add auto refresh

--- app/src/main/java/com/telkomschools/remoteconfigfirebase/MainActivity.kt
+++ app/src/main/java/com/telkomschools/remoteconfigfirebase/MainActivity.kt
@@ -2,6 +2,7 @@ package com.telkomschools.remoteconfigfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.View
 import android.widget.TextView
@@ -26,6 +27,29 @@ class MainActivity : AppCompatActivity() {
 
         remoteConfig.setDefaultsAsync(R.xml.remote_config_default)
 
+        refresh()
+
+    }
+
+    private fun refresh() {
+        Handler().postDelayed({
+            remoteConfig.fetchAndActivate()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val updated = task.result
+                        val msg = remoteConfig.getString("message")
+                        val tv: TextView = findViewById(R.id.status)
+                        tv.text = msg
+                        Log.d("TAG", "Config params updated: $updated")
+                        Toast.makeText(this, "Fetch and activate succeeded",
+                            Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "Fetch failed",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+            refresh()
+        },10000)
     }
 
     fun fetch(view: View) {
